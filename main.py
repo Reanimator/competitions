4c9a0cb358fb3b4f7f8e45e43409cc9c20770312
Reanimator <ageev.mikhail.23@gmail.com>
2022-01-26 14:13:58 +0100

    Add description

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def find_athlets(know_english, sportsmen, more_than_20_years):
+    """
+    Функция для отбора на соревнования
+    :param know_english: список хорошо владебщих английским
+    :param sportsmen: список спортсменов
+    :param more_than_20_years: список стедентов старше 20
+    :return: список подходящих для соревнования
+    """
     return [student for student in know_english if student in sportsmen and student in more_than_20_years]
 
 
